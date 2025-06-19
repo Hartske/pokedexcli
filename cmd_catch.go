23247ebe62f3fe9b780d9c3ea8be8d9f232697d0
Hartske <keatoru@gmail.com>
2025-06-19 10:13:23 -0700

    updated catch command with some formatting and animation?

--- cmd_catch.go
+++ cmd_catch.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 func commandCatch(cfg *config, args ...string) error {
@@ -22,9 +23,20 @@ func commandCatch(cfg *config, args ...string) error {
 
 	catchRes := cfg.pokeapiClient.CatchPokemon(exp)
 
+	fmt.Println("")
+	time.Sleep(1 * time.Second)
+	fmt.Print(".")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Print(".\n")
+	time.Sleep(1 * time.Second)
+	fmt.Println("")
+
 	if catchRes {
 		cfg.pokedex.Add(name, pokemonRes)
 		fmt.Printf("%s was caught!\n", name)
+		fmt.Println("You can now inspect this pokemon!")
 	} else {
 		fmt.Printf("%s escaped!\n", name)
 	}
