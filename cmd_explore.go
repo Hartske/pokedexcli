85f3e95ecae66fab34314ade453b0b8bde40c271
Hartske <keatoru@gmail.com>
2025-06-18 20:51:01 -0700

    updated explore guardrail

--- cmd_explore.go
+++ cmd_explore.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandExplore(cfg *config, args ...string) error {
+	if len(args) < 1 {
+		return errors.New("you must provide a location name")
+	}
 	locationRes, err := cfg.pokeapiClient.GetLocation(&args[0])
 	if err != nil {
 		return err
 	}
 	fmt.Printf("Exploring %s\n", locationRes.Name)
-	fmt.Println("Found Pokemon:")
+	fmt.Println("Found Pokemon: ")
 	for _, pokemon := range locationRes.PokemonEncounters {
 		fmt.Printf(" - %s\n", pokemon.Pokemon.Name)
 	}
